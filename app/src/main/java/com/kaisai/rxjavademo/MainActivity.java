78f84e70c9640fe0677ed6c34b8aed37154d0db9
mayinfeng <mayinfeng@youguu.com>
2016-10-14 14:20:09 +0800

    操作符

--- app/src/main/java/com/kaisai/rxjavademo/MainActivity.java
+++ app/src/main/java/com/kaisai/rxjavademo/MainActivity.java
@@ -3,6 +3,7 @@ package com.kaisai.rxjavademo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,13 +11,15 @@ import java.util.List;
 
 import rx.Observable;
 import rx.Subscriber;
-import rx.functions.Action1;
 import rx.functions.Func1;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Subscriber<String> sur;
+
+    private final static String TAG = MainActivity.class.getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -97,26 +100,79 @@ public class MainActivity extends AppCompatActivity {
          *
          * 理解：flatMap输出的新的Observable正是我们在Subscriber想要接收的
          * */
-        query("hello guy").flatMap(new Func1<List<String>, Observable<String>>() {
+       /* query("hello guy").flatMap(new Func1<List<String>, Observable<String>>() {
             @Override
             public Observable<String> call(List<String> strings) {
                 return Observable.from(strings);
             }
-        }).subscribe(new Action1<String>() {
+        }).flatMap(new Func1<String, Observable<String>>() {
+            @Override
+            public Observable<String> call(String s) {
+                return getTitle(s);
+            }
+        }).filter(new Func1<String, Boolean>() {        //filter()是输出和输入相同的元素，并且会过滤掉那些不满足检查条件的
+            @Override
+            public Boolean call(String s) {
+                return s!="404";
+            }
+        }).take(5).doOnNext(new Action1<String>() {
             @Override
             public void call(String s) {
+                //saveTitle();
+            }
+        }).subscribe(new Action1<String>() {    //take()输出最多指定数量的结果，次数大于源Observable发出的，全部输出
+            @Override
+            public void call(String s) {
+                Log.e(TAG, "call: "+s);
+            }
+        });*/
 
+        Observable.just("Hello world")
+                .map(new Func1<String, String>() {
+                    @Override
+                    public String call(String s) {
+                        //throw new RuntimeException("11111");
+                        return s;
+                    }
+                }).map(new Func1<String, String>() {
+                    @Override
+                    public String call(String s) {
+                         throw new RuntimeException("22222");
+                    }
+        }).subscribe(new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                Log.e(TAG, "onCompleted: ");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "ouch!: ");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.e(TAG, "next: "+s);
             }
         });
 
+
     }
 
-    public Observable<List<String>> query(String str){
+    public Observable<List<String>> query(String str) {
         List<String> list = new ArrayList<>();
-        if(str.contains("hello")){
-            list = Arrays.asList("www.baidu.com","www.sina.com","www.360.cn");
+        if (str.contains("hello")) {
+            list = Arrays.asList("www.baidu.com", "***.sina.***", "www.360.cn");
         }
         return Observable.just(list);
+    }
+
+    public Observable<String> getTitle(String url) {
+        if (url.contains("www")) {
+            return Observable.just("this is a website");
+        } else {
+            return Observable.just("404");
+        }
     }
 }
 
